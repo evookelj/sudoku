46035d6aed07d621762a2fa828a82a8d38d783d7
evookelj <evukelj@stuy.edu>
2017-08-02 11:25:36 -0400

    Can find a few lines?

--- load_puzzle.py
+++ load_puzzle.py
@@ -1,23 +1,51 @@
 import cv2
 import numpy as np
+from math import pi
+import matplotlib.pyplot as plt
 
-# prep image
-img =  cv2.imread('puzl.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray,(5,5),0)
-thresh = cv2.adaptiveThreshold(gray,255,1,1,11,2)
+ratio2 = 3
+kernel_size = 3
+lowThreshold = 30
 
-# find puzzle area within img
-image, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-biggest = None
-max_area = 0
-for i in contours:
-	area = cv2.contourArea(i)
-	if area > 100:
-			peri = cv2.arcLength(i,True)
-			approx = cv2.approxPolyDP(i,0.02*peri,True)
-			if area > max_area and len(approx)==4:
-					biggest = approx
-					max_area = area
+frame = cv2.imread('puzl.jpg')
+sudoku1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+sudoku1 = cv2.blur(sudoku1, (3,3))
+edges = cv2.Canny(sudoku1, lowThreshold, lowThreshold*ratio2, kernel_size)
 
-# todo: extract puzzle from puzzle area
+# Apply Hough Line Transform, return a list of rho and theta
+cv_pi = pi
+thresh = 10
+lines = cv2.HoughLines(edges, 2, cv_pi/180, 300, 0, 0)
+lines = [line[0] for line in lines]
+print len(lines)
+if (lines is not None):
+
+	# Define the position of horizontal and vertical line
+	pos_hori = 0
+	pos_vert = 0
+	for rho,theta in lines:
+		a = np.cos(theta)
+		b = np.sin(theta)
+		x0 = a*rho
+		y0 = b*rho
+		x1 = int(x0 + 1000*(-b))
+		y1 = int(y0 + 1000*(a))
+		x2 = int(x0 - 1000*(-b))
+		y2 = int(y0 - 1000*(a))
+
+		# If b > 0.5, the angle must be greater than 45 degree
+		# so we consider that line as a vertical line
+		if (b>0.5):
+		# Check the position
+			if(rho-pos_hori>thresh):
+				# Update the positionpos_hori=rho
+				pos_hori=rho
+				cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+		else:
+			if(rho-pos_vert>thresh):
+				pos_vert=rho
+				cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),2)
+
+		plt.imshow(frame)
+		plt.title('img')
+		plt.show()
